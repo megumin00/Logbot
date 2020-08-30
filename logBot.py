79478bf1276dc47affcb4244c2889e8f5a95247e
megumin00 <43295199+megumin00@users.noreply.github.com>
2020-08-30 14:33:01 +0800

    Update logBot.py

--- logBot.py
+++ logBot.py
@@ -8,6 +8,7 @@ Created on Thu Aug 27 15:02:02 2020
 import discord
 import nest_asyncio
 import json
+import asyncio
 from discord.ext import commands
 
 class discBot():
@@ -35,7 +36,7 @@ class discBot():
         with open("servers.json", "w") as outfile: 
             outfile.write(json_object)
         
-    def trustedCommands(self):                
+    def trustedCommands(self):       
         @bot.command()
         async def bindserver(message):
             self.readJson()
@@ -47,32 +48,7 @@ class discBot():
                 masterDict = {int(splitContent[1]) : message.channel.id}
                 self.writeJson(masterDict)
                 
-        @bot.command()
-        async def help(message):
-            if message.author.id in self.trusted:
-                helpOwner = discord.Embed(color=0xCAFFFF, title="Bot Commands")
-                helpOwner.set_author(name='Help has arrived :D',
-                icon_url='https://cdn.discordapp.com/attachments/748479776666419223/749449403080900698/chillstolfo.png')
-                    
-                helpOwner.add_field(name=";help", value='prints out this menu', inline=True)
-                helpOwner.add_field(name=";bindserver (server ID)", value='binds all messages from that server to be redirected to this channel (needs bot inside that server)', inline=True)
-                helpOwner.add_field(name=';displayactive', value='displays all active servers for this channel', inline=True)
-                helpOwner.set_footer(text="this only exists because I don't comment my code :c")
-                    
-                await message.channel.send(embed=helpOwner)
-                
-                helpAdmin = discord.Embed(color=0xFFFFCA, title="Admin Commands")
-                helpAdmin.add_field(name=';clear (x)', value='clears x amount of messages in chat', inline=True )
-                helpAdmin.set_footer(text="this only exists because I don't comment my code :c")
-                
-                await message.channel.send(embed=helpAdmin)
-                
-                helpUser = discord.Embed(color=0xBDFFBD, title='User Commands')
-                helpUser.add_field(name=";ping", value='pong?', inline=True )
-                helpUser.set_footer(text="this only exists because I don't comment my code :c")
-                
-                await message.channel.send(embed=helpUser)
-                
+        
                 
                 
         @bot.command()
@@ -88,15 +64,61 @@ class discBot():
                     
                 await message.channel.send(embed=self.displayActive)
                     
-                '''
+                
     def adminCommands(self):
         @bot.command()
-        async def clear(message, args):
+        async def clear(message, arg):
+            messages = []
             
+            async for i in message.history(limit=int(arg)+1):
+                
+                messages.append(i)
+                
+            await message.channel.delete_messages(messages)
+            await message.channel.send('Cleared `{}` Message(s)'.format(arg))
+
+            async for i in message.history(limit=1):
+                selfClear = [i]
+                await asyncio.sleep(1)
+                await message.channel.delete_messages(selfClear)
+                
         
+            
+            
+    def userCommands(self):
+        @bot.command()
+        async def ping(message):
+            await message.channel.send('pong')
         
-    def userCommands(self):'''
-        
+        @bot.command()
+        async def help(message):
+            helpOwner = discord.Embed(color=0xCAFFFF, title="Bot Commands")
+            helpOwner.set_author(name='Help has arrived :D',
+            icon_url='https://cdn.discordapp.com/attachments/748479776666419223/749449403080900698/chillstolfo.png')
+            
+            helpOwner.add_field(name=";bindserver (server ID)", value='binds all messages from that server to be redirected to this channel (needs bot inside that server)', inline=True)
+            helpOwner.add_field(name=';displayactive', value='displays all active servers for this channel', inline=True)
+            helpOwner.set_footer(text="this only exists because I don't comment my code :c")
+            
+            await message.channel.send(embed=helpOwner)
+            
+            helpAdmin = discord.Embed(color=0xFFFFCA, title="Admin Commands")
+            helpAdmin.add_field(name=';clear (x)', value='clears x amount of messages in chat', inline=True )
+            helpAdmin.set_footer(text="this only exists because I don't comment my code :c")
+            
+            await message.channel.send(embed=helpAdmin)
+            
+            helpUser = discord.Embed(color=0xBDFFBD, title='User Commands')
+            helpUser.add_field(name=";help", value='prints out this menu', inline=True)
+            helpUser.add_field(name=";ping", value='pong?', inline=True )
+            helpUser.set_footer(text="this only exists because I don't comment my code :c")
+            
+            await message.channel.send(embed=helpUser)
+            
+            moreInfo = discord.Embed(title='https://github.com/megumin00/authoritarian-dictatorship-discbot')
+            
+            await message.channel.send(embed=moreInfo)
+                
         
             
     def serverLog(self): 
@@ -111,7 +133,7 @@ class discBot():
                     embedVar = discord.Embed(title="{}".format(message.content), color=0xFFCAFF)
                     embedVar.set_author(name="Message sent in {}, (ID:{})".format(message.guild, message.guild.id),
                     icon_url='https://cdn.discordapp.com/attachments/748479776666419223/749449403080900698/chillstolfo.png')
-                    embedVar.add_field(name="Channel: {}, (ID:{})".format(message.channel, message.channel.id), value='by <@{}>, <#{}> (ID:{})'.format(message.author.id, message.channel.id, message.author.id), inline=True)
+                    embedVar.add_field(name="Channel: {}, (ID:{})".format(message.channel, message.channel.id), value='by <@!{}>, <#{}> (ID:{})'.format(message.author.id, message.channel.id, message.author.id), inline=True)
     
                     await loggedChannelGet.send(embed=embedVar)
                         
@@ -121,7 +143,7 @@ class discBot():
             
     def run(self):
         self.trustedCommands()
-        self.adminComamnds()
+        self.adminCommands()
         self.userCommands()
         self.serverLog()
         
